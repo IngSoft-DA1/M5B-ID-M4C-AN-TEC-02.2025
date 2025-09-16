65c916fd6a5cf9f84bcf2aca4ab629ade5df6ca8
TomasBanales <tomasban12@gmail.com>
2025-09-16 20:36:14 -0300

    add blazor example

---
+++ examples/blazor/DA12025/DataAccess/InMemoryDatabase.cs
@@ -0,0 +1,59 @@
+﻿using Domain;
+
+namespace DataAccess
+{
+    public class InMemoryDatabase
+    {
+        private List<Movie> _movies;
+
+        public InMemoryDatabase()
+        {
+            _movies = new List<Movie>();
+            LoadDefaultMovies();
+        }
+
+        private void LoadDefaultMovies()
+        {
+            _movies.Add(new Movie("Cast Away", "Robert Zemeckis", new DateTime(2000, 12, 22)));
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return _movies;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _movies.Add(movie);
+        }
+
+        public Movie GetMovie(string title)
+        {
+            foreach (var movieInList in _movies)
+            {
+                if (movieInList.Title.Equals(title))
+                {
+                    return movieInList;
+                }
+            }
+            return null;
+        }
+        
+        public void RemoveMovie(Movie movie)
+        {
+            _movies.Remove(movie);
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            foreach (var movieInList in _movies)
+            {
+                movieInList.Title = movie.Title;
+                movieInList.ReleaseDate = movie.ReleaseDate;
+                movieInList.Director = movie.Director;
+            }
+        }
+
+        
+    }
+}
