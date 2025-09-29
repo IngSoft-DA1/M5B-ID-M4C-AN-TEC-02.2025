3e70b773524ab1c675c9cb0dabca37ffa08a7d76
marcos <marcosbuydid@gmail.com>
2025-09-29 19:20:09 -0300

    add login example

---
+++ examples/blazor-login-example/DA12025/DataAccess/InMemoryDatabase.cs
@@ -0,0 +1,62 @@
+﻿using Domain;
+
+namespace DataAccess
+{
+    public class InMemoryDatabase
+    {
+        private List<Movie> Movies { get; }
+        private List<User> Users { get; }
+
+        public InMemoryDatabase()
+        {
+            Movies = new List<Movie>();
+            Users = new List<User>();
+            LoadDefaultMovies();
+            LoadDefaultAdministratorUser();
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return Movies;
+        }
+
+        public Movie? GetMovie(string title)
+        {
+            Movie? movie = Movies.FirstOrDefault(movie => movie.Title == title);
+            return movie;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            Movies.Add(movie);
+        }
+
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+
+        public User? GetUser(string email)
+        {
+            User? user = Users.FirstOrDefault(user => user.Email == email);
+            return user;
+        }
+
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+        private void LoadDefaultMovies()
+        {
+            
+            Movies.Add(new Movie("Cast Away", "Robert Zemeckis", new DateTime(2000, 12, 22), 25000000));
+        }
+
+        private void LoadDefaultAdministratorUser()
+        {
+            Users.Add(new User("Alumno", "Ort", "alumno@ort.edu.uy", "123456", "User"));
+        }
+    
+    }
+}
---
+++ examples/blazor-login-example/DA12025/Services/Models/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace Services.Models;
+
+public class UserDTO
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    
+    public UserDTO() {}
+
+    public UserDTO(string name, string lastName, string email, string password, string role)
+    {
+        Name = name;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+}
