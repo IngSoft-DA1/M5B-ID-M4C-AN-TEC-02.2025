52f413ad452af74242457848762c73c294f6a830
marcos <marcosbuydid@gmail.com>
2025-10-27 22:32:16 -0300

    add ef repository test example

---
+++ examples/entity_framework_repository_tests/DA12025/DataAccess/MovieRepository.cs
@@ -0,0 +1,64 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class MovieRepository
+{
+    protected readonly AppDbContext _appDbContext;
+
+    public MovieRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public List<Movie> GetMovies()
+    {
+        return _appDbContext.Set<Movie>().AsQueryable<Movie>().ToList();
+    }
+
+    public Movie? GetMovie(Func<Movie, bool> filter)
+    {
+        return _appDbContext.Set<Movie>().FirstOrDefault(filter);
+    }
+
+    public void AddMovie(Movie movie)
+    {
+        try
+        {
+            _appDbContext.Set<Movie>().Add(movie);
+            _appDbContext.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            throw new DbUpdateException("Cannot add the movie", e);
+        }
+    }
+
+    public void DeleteMovie(Movie movie)
+    {
+        try
+        {
+            _appDbContext.Set<Movie>().Remove(movie);
+            _appDbContext.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            throw new DbUpdateException("Cannot delete the movie", e);
+        }
+    }
+
+    public void UpdateMovie(Movie movie)
+    {
+        try
+        {
+            _appDbContext.Update(movie);
+            _appDbContext.Entry<Movie>(movie).State = EntityState.Modified;
+            _appDbContext.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            throw new DbUpdateException("Cannot update the movie", e);
+        }
+    }
+}
---
+++ examples/entity_framework_repository_tests/DA12025/Services/MovieService.cs
@@ -0,0 +1,73 @@
+﻿using Domain;
+using DataAccess;
+using Services.Interfaces;
+using Services.Models;
+
+namespace Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly MovieRepository _movieRepository;
+
+        public MovieService(MovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public void AddMovie(MovieDTO movie)
+        {
+            ValidateUniqueTitle(movie.Title);
+            _movieRepository.AddMovie(ToEntity(movie));
+        }
+
+        public List<MovieDTO> GetMovies()
+        {
+            List<MovieDTO> moviesDTO = new List<MovieDTO>();
+
+            foreach (var movie in _movieRepository.GetMovies())
+            {
+                moviesDTO.Add(FromEntity(movie));
+            }
+
+            return moviesDTO;
+        }
+
+
+        public MovieDTO GetMovie(string title)
+        {
+            Movie? movie = _movieRepository.GetMovie(movie => movie.Title == title);
+            if (movie == null)
+            {
+                throw new ArgumentException("Cannot find movie with this title");
+            }
+
+            return FromEntity(movie);
+        }
+
+        private void ValidateUniqueTitle(string title)
+        {
+            foreach (var movie in _movieRepository.GetMovies())
+            {
+                if (movie.Title == title)
+                {
+                    throw new ArgumentException("There`s a movie already defined with that title");
+                }
+            }
+        }
+
+        private static Movie ToEntity(MovieDTO movieDTO)
+        {
+            return new Movie(movieDTO.Id, movieDTO.Title, movieDTO.Director, movieDTO.ReleaseDate, movieDTO.Budget);
+        }
+
+        private static MovieDTO FromEntity(Movie movie)
+        {
+            return new MovieDTO()
+            {
+                Title = movie.Title,
+                Director = movie.Director,
+                ReleaseDate = movie.ReleaseDate
+            };
+        }
+    }
+}
